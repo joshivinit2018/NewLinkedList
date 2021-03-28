02a8b6d3982a17dc84640464db94d2baee631a53
joshivinit2018 <joshivinit2018@gmailcom>
2021-03-28 08:41:58 +0530

    [vinit] : add add linkedListTop

--- src/main/java/com/bridgelabz/LinkedList.java
+++ src/main/java/com/bridgelabz/LinkedList.java
@@ -1,34 +1,47 @@
 package com.bridgelabz;
 
 public class LinkedList<K> {
-    private LinkedList next;
-    private K  key;
-
-    //Constructor Declaration
-    public LinkedList(K key){
-        this.key = null;
-        this.next = null;
-    }
-
-
     public K getNext() {
-        return null;
     }
 
+    public class LinkedList<K> implements INode<K>{
 
-    public void setNext(LinkedList<K> mySecondNode) {
-    this.key=key;
-    }
-
-    public void LinkedList getNext() {
-        return next;
-    }
-
-    //To set Next
-    public void setNext(LinkedList next) {
-        this.next = next;
-    }
+        private LinkedList next;
+        private LinkedList<K> next;
+        private K  key;
 
 
+        //Constructor Declaration
+        public LinkedList(K key){
+            this.key = null;
+            this.key = key;
+            this.next = null;
+        }
 
+        //to get key
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        //set key
+        @Override
+        public void setKey(K key){
+            this.key = key;
+        }
+
+        //to get Next
+        @Override
+        public LinkedList getNext() {
+            return next;
+        }
+
+        //To set Next
+        public void setNext(LinkedList next) {
+            this.next = next;
+            @Override
+            public void setNext(INode next) {
+                this.next = (LinkedList<K>) next;
+
+            }
 }
